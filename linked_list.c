972cd8305565c50c0f3fd18e1d23a6873951e072
kevink2023 <kevink2029@gmail.com>
2022-04-23 18:51:11 -0400

    linked list from simulator lab

--- linked_list.c
+++ linked_list.c
@@ -1,68 +1,214 @@
 #include "linked_list.h"
 
 // Creates and returns a new list
-list_t* list_create()
+// If compare is NULL, list_insert just inserts at the head
+list_t* list_create(compare_fn compare)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
-    return NULL;
+    list_t * new_list = malloc(sizeof(list_t));
+
+    new_list->head = NULL;
+    new_list->tail = NULL;
+    new_list->count = 0;
+    new_list->compare = compare;
+
+    return new_list;
 }
 
 // Destroys a list
 void list_destroy(list_t* list)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
+    list_node_t * node = list_head(list);
+
+    while (node != NULL)
+    {
+        list_remove(list, node);
+        node = list_head(list);
+    }
+    
+    free(list);
 }
 
-// Returns beginning of the list
-list_node_t* list_begin(list_t* list)
+// Returns head of the list
+list_node_t* list_head(list_t* list)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
-    return NULL;
+    return list->head;
+}
+
+// Returns tail of the list
+list_node_t* list_tail(list_t* list)
+{
+    return list->tail;
 }
 
 // Returns next element in the list
 list_node_t* list_next(list_node_t* node)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
-    return NULL;
+    return node->next;
+}
+
+// Returns prev element in the list
+list_node_t* list_prev(list_node_t* node)
+{
+    return node->prev;
+}
+
+// Returns end of the list marker
+list_node_t* list_end(list_t* list)
+{
+    return list->tail;
 }
 
 // Returns data in the given list node
 void* list_data(list_node_t* node)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
-    return NULL;
+    return node->data;
 }
 
 // Returns the number of elements in the list
 size_t list_count(list_t* list)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
-    return 0;
+    return list->count;
 }
 
 // Finds the first node in the list with the given data
 // Returns NULL if data could not be found
 list_node_t* list_find(list_t* list, void* data)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
+    list_node_t * node = list_head(list);
+    int           i;
+
+    for(i = 0; i < list_count(list); i++)
+    {
+        if(list_data(node) == data)
+        {
+            return node;
+        }
+
+        node = list_next(node);
+    }
+    
     return NULL;
 }
 
 // Inserts a new node in the list with the given data
-void list_insert(list_t* list, void* data)
+// Returns new node inserted
+list_node_t* list_insert(list_t* list, void* data)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
+    list_node_t * new_node  = malloc(sizeof(list_node_t));
+    list_node_t * comp_node = list_head(list);
+    new_node->data = data;
+
+    if (comp_node == NULL)
+    {
+        // Empty list
+        list->head = new_node;
+        list->tail = new_node;
+        new_node->next = NULL;
+        new_node->prev = NULL;
+        list->count++;
+        return new_node;
+    }
+    else if (list->compare != NULL)
+    {
+        int i, ret;
+        for(i = 0; i < list_count(list); i++)
+        {
+            ret = list->compare(new_node->data, comp_node->data);
+
+            if(ret == -1)      // Place new node before comp node
+            {
+                new_node->next  = comp_node;
+                
+                if(comp_node == list_head(list)){   
+                    new_node->prev          = NULL;
+                    list->head              = new_node;
+                } else {
+                    new_node->prev          = comp_node->prev;
+                    comp_node->prev->next   = new_node;
+                }
+
+                comp_node->prev = new_node;
+                
+                list->count++;
+                return new_node;
+                
+            }
+            else if (ret == 0)  // Arbritrary, but place new node after comp node
+            {                            
+                new_node->prev  = comp_node;
+                
+                if(comp_node == list_tail(list)){
+                    new_node->next          = NULL;
+                    list->tail              = new_node;      // Update tail if necessary   
+                } else {
+                    new_node->next          = comp_node->next;
+                    comp_node->next->prev   = new_node;
+                }
+
+                comp_node->next = new_node;
+
+                list->count++;
+                return new_node;
+            }
+            else 
+            {      
+                comp_node = list_next(comp_node);   // Check next node
+            }
+        }
+        // If here, insert after tail
+        comp_node       = list_tail(list);
+        new_node->prev  = comp_node;
+        new_node->next  = NULL;
+        list->tail      = new_node;
+        comp_node->next = new_node;
+        list->count++;
+        return new_node;
+    } 
+    else // No comp function so just place it at beginnging of the list
+    {
+        new_node->next  = comp_node;
+        new_node->prev  = NULL;
+        list->head      = new_node;
+        comp_node->prev = new_node;
+        list->count++;
+        return new_node;
+    }
 }
 
 // Removes a node from the list and frees the node resources
 void list_remove(list_t* list, list_node_t* node)
 {
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
+    // Will crash if 
+    //  1) list is empty
+    //  2) node is invalid
+    // This would be user error
+    if (node == list_head(list))
+    {
+        if(node == list_tail(list)){    // Head and tail
+            list->head = NULL;
+            list->tail = NULL;
+        } else {                        // Head, not tail
+            list->head = node->next;
+            node->next->prev = NULL;
+        }        
+    }
+    else
+    {
+        if(node == list_tail(list)){    // Not head, tail
+            list->tail = node->prev;
+            node->prev->next = NULL;
+        } else {                        // Not head, not tail
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+        }    
+    } 
+
+    list->count--;
+    free(node);  
 }
 
 // Executes a function for each element in the list
-void list_foreach(list_t* list, void (*func)(void* data))
-{
-    /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
-}
+// void list_foreach(list_t* list, void (*func)(void* data))
+// {
+//     /* IMPLEMENT THIS IF YOU WANT TO USE LINKED LISTS */
+// }
--- linked_list.h
+++ linked_list.h
@@ -1,31 +1,49 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
+
+// Compares data1 and data2 and returns
+// -1 if data1 goes before data2
+// 0 if data1 is equivalent to data2
+// 1 if data1 goes after data2
+typedef int (*compare_fn)(void* data1, void* data2);
 
 typedef struct list_node {
-    struct list_node* next;
-    struct list_node* prev;
-    void* data;
+    struct list_node* next; // next node in list
+    struct list_node* prev; // prev node in list
+    void* data; // generic user-specified data pointer
 } list_node_t;
 
 typedef struct {
-    list_node_t* head;
-    size_t count;
+    list_node_t* head; // head of the list
+    list_node_t* tail; // tail of the list
+    size_t count; // count of nodes in the list
+    compare_fn compare; // order for inserting data; NULL indicates to insert at the head
 } list_t;
 
 // Creates and returns a new list
-list_t* list_create();
+// If compare is NULL, list_insert just inserts at the head
+list_t* list_create(compare_fn compare);
 
 // Destroys a list
 void list_destroy(list_t* list);
 
-// Returns beginning of the list
-list_node_t* list_begin(list_t* list);
+// Returns head of the list
+list_node_t* list_head(list_t* list);
+
+// Returns tail of the list
+list_node_t* list_tail(list_t* list);
 
 // Returns next element in the list
 list_node_t* list_next(list_node_t* node);
+
+// Returns prev element in the list
+list_node_t* list_prev(list_node_t* node);
+
+// Returns end of the list marker
+list_node_t* list_end(list_t* list);
 
 // Returns data in the given list node
 void* list_data(list_node_t* node);
@@ -38,12 +56,13 @@ size_t list_count(list_t* list);
 list_node_t* list_find(list_t* list, void* data);
 
 // Inserts a new node in the list with the given data
-void list_insert(list_t* list, void* data);
+// Returns new node inserted
+list_node_t* list_insert(list_t* list, void* data);
 
 // Removes a node from the list and frees the node resources
 void list_remove(list_t* list, list_node_t* node);
 
-// Executes a function for each element in the list
-void list_foreach(list_t* list, void (*func)(void* data));
+// // Executes a function for each element in the list
+// void list_foreach(list_t* list, void (*func)(void* data));
 
 #endif // LINKED_LIST_H
